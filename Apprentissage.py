e8bf1ead2307b3b2d395a3ff6ec8c006f344a7d1
emmanuelguiro <emmanuelguiro47@gmail.com>
2026-01-17 21:06:09 +0000

    ajout d'une calculatrice basique

--- Apprentissage.py
+++ Apprentissage.py
@@ -1,1 +1,10 @@
-print( ' Hello world ' )
+nombre1 = input("Entrez le premier nombre !")
+nombre2 = input("Entrez le second nombre !")
+if not (isnumeric.nombre1() and isnumeric.nombre2)
+raise SystemExit("Fin du programme")
+nombre1 = int(nombre1) 
+nombre2 = int(nombre2)
+operation = input("Entrez le symbole de votre operation!")
+if operation not in ["+","-","*","/"]
+if operation == "/" and nombre2 == "0"
+raise SystemExit("Cette operation est impossible")
