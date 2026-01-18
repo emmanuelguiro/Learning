af940252425ff334fcd3e3ee66aba82c32c445c8
emmanuelguiro <emmanuelguiro47@gmail.com>
2026-01-18 01:46:45 +0000

    make a basic calculator

--- Apprentissage.py
+++ Apprentissage.py
@@ -17,19 +17,3 @@ else :
     resultats = nombre1 / nombre2
     resultats = round(resultats, 3)
     print(f"Le resultat est : {nombre1},{operation},{nombre2} ={resultats}")
-
-
-# Quand tu as des erreurs et tu ne sais d'où ça vient
-# si le code n'est pas beaucoup
-# tu peu metre eb commentaire et aller etape par etape pour voir où ça coince.
-
-# mais dans ton cas c'est deja claire:
-#  File "C:\Learning\Apprentissage.py", line 3 : il te donne 
-#  le nom du fichier qui pause pb et il te donne la ligne qui pause pb
-
-# if not (nombre1.isnumeric() and nombre2.is.numeric())
-# il te précise que dans cette ligne c'est le "is" qui pause pb
-
-# donc corrige!
-# fait moi signe par whasapp si le message d'erreur change
-#  quand tu auras fini de corriger ce pb
