eafb6798bd82270b49305832cf72ed998393667a
emmanuelguiro <emmanuelguiro47@gmail.com>
2026-01-18 01:48:24 +0000

    make a basic calculator

--- Apprentissage.py
+++ Apprentissage.py
@@ -1,10 +1,10 @@
-nombre1 = input("Entrez le premier nombre ! ")
-nombre2 = input("Entrez le second nombre ! ")
+nombre1 = input("Entrez le premier nombre: ")
+nombre2 = input("Entrez le second nombre: ")
 if not (nombre1.isnumeric() and nombre2.isnumeric()):
     raise SystemExit("Fin du programme, entrez un nombre entier.")
 nombre1 = int(nombre1) 
 nombre2 = int(nombre2)
-operation = input("Entrez le symbole de votre operation! ")
+operation = input("Entrez le symbole de votre operation: ")
 if operation not in ["+","-","*","/"] and operation == "/" and nombre2 == "0":
     raise SystemExit("Cette operation est impossible")
 if operation == "+" :
